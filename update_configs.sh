eb082142732881015dfdbad83950334bbacd76f0
Alfredo <asuarezaceves@gmail.com>
2024-03-12 13:54:26 +0100

    adding an updated backed-up script functionality

--- update_configs.sh
+++ update_configs.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-# Update the .zshrc file with the correct paths
-rm -rf ~/.zshrc
+set -e # Exit immediately if a command exits with a non-zero status.
+
+# Backup and update the .zshrc file
+if [ -f ~/.zshrc ]; then
+    cp ~/.zshrc ~/.zshrc.backup
+    echo "Backed up the original .zshrc to .zshrc.backup"
+fi
 cp .zshrc ~/.zshrc
 echo "Updated .zshrc file with the correct paths"
 
-# update neovim configuration
-rm -rf ~/.config/nvim/
+# Update Neovim configuration
+if [ -d ~/.config/nvim ]; then
+    rm -rf ~/.config/nvim.backup
+    mv ~/.config/nvim ~/.config/nvim.backup
+    echo "Backed up the original Neovim config to nvim.backup"
+fi
+mkdir -p ~/.config
 cp -r nvim ~/.config/
-echo "Updated neovim configuration"
+echo "Updated Neovim configuration"
 
-# update tmux configuration
-rm -rf ~/.tmux.conf
+# Update tmux configuration
+if [ -f ~/.tmux.conf ]; then
+    cp ~/.tmux.conf ~/.tmux.conf.backup
+    echo "Backed up the original .tmux.conf to .tmux.conf.backup"
+fi
 cp .tmux.conf ~/.tmux.conf
 echo "Updated tmux configuration"
 
-
-echo "trigger exec zsh to resource the zshrc file :), thanks and goodbye"
+echo "Please remember to 'exec zsh' or start a new shell session to apply the updated configurations. Thanks and goodbye!"
 
